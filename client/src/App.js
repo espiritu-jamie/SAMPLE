144387c6127e4198c3b7be4d14d5d8e7f914ec7c
BensRandomStash <119640465+BensRandomStash@users.noreply.github.com>
2024-02-29 08:06:30 -0700

    Business Homepage

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Spinner from "./components/Spinner";
 import AdminAllAppointments from "./pages/admin/adminAllAppointments";
 
 import HomePage from "./pages/HomePage";
+import BusinessHome from "./pages/BusinessHome";
 import Login from "./pages/Login";
 import NotificationPage from "./pages/NotificationPage";
 import Register from "./pages/Register";
@@ -83,6 +84,14 @@ function App() {
                   <Login />
                 </PublicRoute>
               }
+            />
+            <Route 
+            path="/BusinessHome"
+            element={
+              <PublicRoute>
+                <BusinessHome />
+              </PublicRoute>
+            }
             />
             <Route
               path="/register"

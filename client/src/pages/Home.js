ac8160aa7a0ed2a09a25526ddb1425301a4f99ac
lancegonzales04 <lancegonzaleszero4@gmail.com>
2024-04-03 16:36:48 -0600

    homapage
    not done testing

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,3 @@
-// Home.js
-// import React from 'react';
-// import Navbar from '../components/Navbar';
-// import '../styles/HomeStyles.css';
-
-// const Home = () => {
-//   return (
-//     <div className="home-container">
-//       <Navbar />
-//       <div className="hero-section">
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home;
 import React from 'react';
 import Navbar from '../components/Navbar';
 import '../styles/HomeStyles.css';
@@ -23,14 +7,14 @@ const Home = () => {
     <div className="home-container">
       <Navbar />
       <div className="hero-section">
-        <div className="hero-content">
-          {/* Place your content here */}
-          <h1>Welcome to Our Website</h1>
-          <p>This is a great place to introduce visitors to your site.</p>
-        </div>
         <div className="hero-image">
-          {/* Replace 'image-url.jpg' with the path to your image */}
-          <img src="/cleaning-made-easy.jpg"/>
+          <img src="/cleaning-made-easy.jpg" alt="Effortless cleaning in action"/>
+        </div>
+        <div className="text-container">
+          <div className="hero-content">
+            <h1>JKL CLEANING SERVICE</h1>
+            <p>We Treat Your House Like Ours</p>
+          </div>
         </div>
       </div>
     </div>

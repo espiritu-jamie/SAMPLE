254fed4319f1c1c22ec64f367030a72961b09a40
Jamie <espiritu_jamie@hotmail.com>
2024-04-04 13:33:02 -0600

    Update adminHoursTracking

--- client/src/pages/admin/adminHoursTracking.js
+++ client/src/pages/admin/adminHoursTracking.js
@@ -15,104 +15,96 @@ const AdminHoursTracking = () => {
     const fetchHoursWorked = async () => {
         const token = localStorage.getItem('token');
         try {
-            const response = await axios.get('/api/appointment', {
+            const response = await axios.get('/api/appointment/confirmed-appointments', { // Update the URL to include the status query
                 headers: {
                     Authorization: `Bearer ${token}`,
                 },
             });
+            console.log('Fetched hours worked:', response.data.data);
             calculateHoursWorked(response.data.data);
         } catch (error) {
             console.error('Error fetching hours worked:', error);
         }
     };
+    
 
-    const calculateHoursWorked = (availabilities) => {
+    const calculateHoursWorked = (appointments) => {
         const now = moment();
         let monthsSet = new Set();
-    
+        
         // Generate all months for the current year or a range of years
         const allMonthsOfYear = [];
         const currentYear = now.year();
         for (let month = 0; month < 12; month++) {
-            // Format as "Month YYYY"
             const monthYearFormat = moment().month(month).year(currentYear).format('MMMM YYYY');
             allMonthsOfYear.push(monthYearFormat);
         }
-    
-        const hoursByEmployee = availabilities.reduce((acc, current) => {
-            const userId = current.user?._id|| 'Unknown';
-            const userName = current.user?.name || 'Unknown'; // Assuming name is within the user object
-            console.log('employeeId:', userId);
-            console.log(current.assignedEmployees);
-            console.log(acc);            
-            const startTime = moment.utc(current.starttime, 'HH:mm');
-            const endTime = moment.utc(current.endtime, 'HH:mm');
-            const appointmentDate = moment.utc(current.date);
-
-            const assignedEmployeeNames = current.assignedEmployees.map(employee => employee.name);
-            console.log('Assigned Employee Names:', assignedEmployeeNames);
-            console.log('Current assigned: ', current.assignedEmployees);
-            
-            if (!acc[userId]) {
-                acc[userId] = {
-                    name: userName, // Store the name
-                    totalHours: 0,
-                    months: {},
-                };
-            }
-
+        
+        const hoursByEmployee = appointments.reduce((acc, appointment) => {
+            const appointmentDate = moment.utc(appointment.date);
             if (appointmentDate.isAfter(now)) {
                 return acc;
             }
             
+            const startTime = moment.utc(appointment.starttime, 'HH:mm');
+            const endTime = moment.utc(appointment.endtime, 'HH:mm');
             const duration = moment.duration(endTime.diff(startTime));
             const hours = duration.asHours();
             const monthYear = appointmentDate.format('MMMM YYYY');
             monthsSet.add(monthYear);
-
-            if (!acc[userId]) {
-                acc[userId] = {
-                    totalHours: 0,
-                    months: {},
-                };
-            }
-
-            if (!acc[userId].months[monthYear]) {
-                acc[userId].months[monthYear] = {
-                    monthYear: monthYear,
-                    hoursWorked: 0,
-                };
-            }
-
-            acc[userId].totalHours += hours;
-            acc[userId].months[monthYear].hoursWorked += hours;
-
+            
+            appointment.assignedEmployees.forEach(employee => {
+                const userId = employee._id.toString();
+                const userName = employee.name;
+                
+                if (!acc[userId]) {
+                    acc[userId] = {
+                        name: userName, // Store the name
+                        totalHours: 0,
+                        months: {},
+                    };
+                }
+                
+                if (!acc[userId].months[monthYear]) {
+                    acc[userId].months[monthYear] = {
+                        monthYear,
+                        hoursWorked: 0,
+                    };
+                }
+                
+                acc[userId].totalHours += hours;
+                acc[userId].months[monthYear].hoursWorked += hours;
+            });
+            
             return acc;
         }, {});
-
+        
         allMonthsOfYear.forEach(monthYear => monthsSet.add(monthYear));
-
+    
         const monthsFilter = Array.from(monthsSet).map(monthYear => ({
             text: monthYear,
             value: monthYear,
         }));
         setMonthsFilter(monthsFilter);
-
+    
         const transformedData = Object.keys(hoursByEmployee).map(userId => ({
             key: userId,
-            userId,
+            name: hoursByEmployee[userId].name,
             totalHours: hoursByEmployee[userId].totalHours.toFixed(2),
-            months: Object.values(hoursByEmployee[userId].months),
+            months: Object.values(hoursByEmployee[userId].months).map(month => ({
+                ...month,
+                hoursWorked: month.hoursWorked.toFixed(2),
+            })),
         }));
-
+    
         setHoursWorked(transformedData);
     };
-
+    
     const columns = [
         {
-            title: 'Employee ID',
-            dataIndex: 'userId', // Changed from '_id' to 'userId'
-            key: 'userId',
+            title: 'Employee Name',
+            dataIndex: 'name',
+            key: 'name',
         },
         {
             title: 'Total Hours Worked',

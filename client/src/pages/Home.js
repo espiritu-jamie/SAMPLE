0f7f475c02e67cd66d3c89a6aff8125a6b23b3e0
Jamie <espiritu_jamie@hotmail.com>
2024-04-04 07:54:27 -0600

    Update Styles

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,10 +8,10 @@ const Home = () => {
   <Navbar />
   <div className="hero-section">
     <img className="hero-image rounded-full" src="/ThreeCleaners.jpg" alt="hero_bg" />
-    <div className=" flex-col">
+    <div className="content-container flex-col">
       <div className="text-logo-container">
         <div className="text-box">
-          <p><b>JKL CLEANING SERVICES</b>We Treat Your House Like Ours</p>
+          <p><b>JKL CLEANING SERVICE</b>We Treat Your House Like Ours</p>
         </div>
         <img src="/jkl.png" alt="JKL Logo" className="top-image" />
       </div>
@@ -19,7 +19,7 @@ const Home = () => {
   </div>
   {/* New container for three divs */}
   <div className='text-bottom'>
-  <p class>HOW JKL CLEANING SERVICES WORKS</p>
+  <p class>HOW JKL CLEANING SERVICE WORKS</p>
   </div>
   <div className="feature-container">
     <div className="feature">

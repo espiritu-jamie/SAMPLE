5a1d08f8ec955c5168d5db01cd799be5f0e4e38b
Jamie <espiritu_jamie@hotmail.com>
2024-04-14 16:30:27 -0600

    Update DataFetching.js

--- client/src/components/DataFetching.js
+++ client/src/components/DataFetching.js
@@ -12,7 +12,7 @@ function DataFetchingComponent() {
                 setData(data);
             })
             .catch(error => console.error('Error fetching data:', error));
-    }, []); // The empty array ensures this effect runs only once after the initial render
+    }, []); 
 
     return (
         <div>
